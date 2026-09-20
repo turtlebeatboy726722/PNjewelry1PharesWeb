837cd2efe708026858d55d24ee5eb39733091d1d
Earth <turtlebeatboy@gmail.com>
2026-09-21 00:44:51 +0700

    fix: disable CDN cache

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store, must-revalidate",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
